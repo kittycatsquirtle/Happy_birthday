a546ba6e67a1ec874f9267e4df051e2a571c8b56
Twyla Paska <mysticpaw24@gmail.com>
2021-05-21 10:34:15 -0500

    initial commit

---
+++ breadboard.py
@@ -0,0 +1,82 @@
+# from gpiozero import LED
+# 
+# led=LED(25)
+# 
+# led.on()
+# led.off()
+
+#from gpiozero import LED
+#from time import sleep
+#
+#led=LED(25)
+# 
+#for i in range(0,5):
+#    led.on()
+#    sleep(1)
+#    led.off()
+#    sleep(1)
+
+
+#from gpiozero import LED
+#from time import sleep
+#led=LED(25)
+ 
+#for i in range(0,3):
+    #led.on()
+    #sleep(.5)
+    #led.off()
+   # sleep(.5)
+#for i in range(0,3):
+    #led.on()
+    #sleep(1)
+    #led.off()
+    #sleep(1)
+     
+#for i in range(0,3):
+    #led.on()
+    #sleep(.5)
+    #led.off()
+    #sleep(.5)
+
+
+from gpiozero import LED
+from time import sleep
+
+led1=LED(2)
+led2=LED(3)
+led3=LED(4)
+#led4=
+#led5=
+#led6=
+#led7=
+
+for i in range(0,3):
+    led1.on()
+    sleep(1)
+    led1.off()
+    sleep(1)
+    led2.on()
+    sleep(1)
+    led2.off()
+    sleep(1)
+    led3.on()
+    sleep(1)
+    led3.off()
+    sleep(1)
+ #   led4.on()
+  #  sleep(1)
+   # led4.off()
+    #sleep(1)
+ #   led5.on()
+  #  sleep(1)
+   # led5.off()
+    #sleep(1)
+ #   led6.on()
+  #  sleep(1)
+   # led6.off()
+    #sleep(1)
+#    led7.on()
+ #   sleep(1)
+  #  led7.off()
+   # sleep(1)
+ 
